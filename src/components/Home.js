2c9294352d521c35d14cd9652bf2affab1987a27
parshva555 <parshvadani@gmail.com>
2023-12-09 19:14:34 +0530

    dding NoteItem in a Separate Note component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,8 @@
 import React from "react";
 import { useContext } from "react";
 import noteContext from "../context/notes/noteContext";
+import Notes from "./Notes";
 const Home = () => {
-  const context = useContext(noteContext)
-  const {notes,setNotes} = context;
   return (
     <div>
       <div className="container my-3">
@@ -48,12 +47,7 @@ const Home = () => {
         </button>
       </form>
       </div>
-      <div className="container">
-      <h2>Your Notes</h2>
-      {notes.map((note)=>{
-        return note.title;
-      })}
-      </div>
+      <Notes/>
     </div>
   );
 };
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -14,30 +14,66 @@ const NoteState = (props) => {
         {
           "_id": "65325fca6d90ff21a6801242",
           "user": "6532467091bda43f38bd5665",
-          "title": "my title",
+          "title": " my title",
           "description": "please wake up early",
           "tag": "personal",
           "date": "2023-10-20T11:08:58.950Z",
           "__v": 0
         },
         {
-          "_id": "6533fa78f2cee55b268433c7",
-          "user": "6532467091bda43f38bd5665",
-          "title": "New Note",
-          "description": "Best Note in the world",
-          "tag": "Youtube",
-          "date": "2023-10-21T16:21:12.553Z",
-          "__v": 0
-        },
-        {
-          "_id": "6573032bd50810dbb0e557bc",
-          "user": "6532467091bda43f38bd5665",
-          "title": "New Note it is ",
-          "description": "parshva dani is te besr",
-          "tag": "Youtube",
-          "date": "2023-12-08T11:51:07.673Z",
-          "__v": 0
-        }
+            "_id": "65325fca6d90ff21a6801242",
+            "user": "6532467091bda43f38bd5665",
+            "title": " my title",
+            "description": "please wake up early",
+            "tag": "personal",
+            "date": "2023-10-20T11:08:58.950Z",
+            "__v": 0
+          },
+          {
+            "_id": "65325fca6d90ff21a6801242",
+            "user": "6532467091bda43f38bd5665",
+            "title": " my title",
+            "description": "please wake up early",
+            "tag": "personal",
+            "date": "2023-10-20T11:08:58.950Z",
+            "__v": 0
+          },
+          {
+            "_id": "65325fca6d90ff21a6801242",
+            "user": "6532467091bda43f38bd5665",
+            "title": " my title",
+            "description": "please wake up early",
+            "tag": "personal",
+            "date": "2023-10-20T11:08:58.950Z",
+            "__v": 0
+          },
+          {
+            "_id": "65325fca6d90ff21a6801242",
+            "user": "6532467091bda43f38bd5665",
+            "title": " my title",
+            "description": "please wake up early",
+            "tag": "personal",
+            "date": "2023-10-20T11:08:58.950Z",
+            "__v": 0
+          },
+          {
+            "_id": "65325fca6d90ff21a6801242",
+            "user": "6532467091bda43f38bd5665",
+            "title": " my title",
+            "description": "please wake up early",
+            "tag": "personal",
+            "date": "2023-10-20T11:08:58.950Z",
+            "__v": 0
+          },
+          {
+            "_id": "65325fca6d90ff21a6801242",
+            "user": "6532467091bda43f38bd5665",
+            "title": " my title",
+            "description": "please wake up early",
+            "tag": "personal",
+            "date": "2023-10-20T11:08:58.950Z",
+            "__v": 0
+          }
       ]
 
       const [notes,setNotes] = useState(notesInitial);
