9eba589d7bc19a366d2d61dbacb996106ec77eba
parshva555 <parshvadani@gmail.com>
2023-12-12 17:20:41 +0530

    Adding Login Component

---
+++ src/components/SignUp.js
@@ -0,0 +1,67 @@
+// import React from "react";
+
+// const SignUp = () => {
+//   return (
+//     <div className="container">
+//       <form>
+//       <div className="mb-3">
+//           <label htmlFor="name" className="form-label">
+//             Name
+//           </label>
+//           <input
+//             type="text"
+//             className="form-control"
+//             id="name"
+//             aria-describedby="emailHelp"
+//             onChange={onChange}
+
+//           />
+//         </div>
+//         <div className="mb-3">
+//           <label htmlFor="email" className="form-label">
+//             Email address
+//           </label>
+//           <input
+//             type="email"
+//             className="form-control"
+//             id="email"
+//             aria-describedby="emailHelp"
+//             onChange={onChange}
+
+//           />
+//         </div>
+
+//         <div className="mb-3">
+//           <label htmlFor="password" className="form-label">
+//             Password
+//           </label>
+//           <input
+//             type="password"
+//             className="form-control"
+//             id="password"
+//             onChange={onChange}
+
+//           />
+//         </div>
+//         <div className="mb-3">
+//           <label htmlFor="cpassword" className="form-label">
+//             Confirm Password
+//           </label>
+//           <input
+//             type="password"
+//             className="form-control"
+//             id="cpassword"
+//             onChange={onChange}
+
+//           />
+//         </div>
+
+//         <button type="submit" className="btn btn-primary">
+//           Submit
+//         </button>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default SignUp;
